c26f011d29e3d492c672c8a8efc9109f0c586d04
Marc <ninjaz.gg@outlook.com>
2023-04-12 13:59:13 -0400

    add test

---
+++ tests/quick_dev.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+use anyhow::Result;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://127.0.0.1:8888")?;
+    hc.do_get("/hello").await?.print().await?;
+    
+    Ok(())
+}
